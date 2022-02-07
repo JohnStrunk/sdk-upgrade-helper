0641b052072c05cffac5363b3b9819aac807f158
John Strunk <jstrunk@redhat.com>
2022-02-07 09:13:21 -0500

    Upgrade to v1.17.0
    Signed-off-by: John Strunk <jstrunk@redhat.com>

--- scaffolding/controllers/replicationdestination_controller.go
+++ scaffolding/controllers/replicationdestination_controller.go
@@ -33,7 +33,7 @@ type ReplicationDestinationReconciler struct {
 // the user.
 //
 // For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.10.0/pkg/reconcile
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.11.0/pkg/reconcile
 func (r *ReplicationDestinationReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	_ = log.FromContext(ctx)
 
--- scaffolding/controllers/replicationsource_controller.go
+++ scaffolding/controllers/replicationsource_controller.go
@@ -33,7 +33,7 @@ type ReplicationSourceReconciler struct {
 // the user.
 //
 // For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.10.0/pkg/reconcile
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.11.0/pkg/reconcile
 func (r *ReplicationSourceReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	_ = log.FromContext(ctx)
 
